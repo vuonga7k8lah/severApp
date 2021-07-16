01f900926d575e8e0f52d19cbbb305b7aa144313
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2021-07-16 11:51:59 +0700

    edit mesage response

--- src/Helpers/Message.php
+++ src/Helpers/Message.php
@@ -8,6 +8,8 @@ class Message
 {
     public static function error($msg, $code)
     {
+        header(self::_build_http_header_string($code));
+        header("Content-Type: application/json");
         return json_encode([
             'status' => 'error',
             'msg'    => $msg,
@@ -15,8 +17,23 @@ class Message
         ]);
     }
 
+    private static function _build_http_header_string($statusCode): string
+    {
+        $status = [
+            200 => 'OK',
+            404 => 'Not Found',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            405 => 'Method Not Allowed',
+            500 => 'Internal Server Error'
+        ];
+        return "HTTP/1.1 " . $statusCode . " " . $status[$statusCode];
+    }
+
     public static function success($msg, $aData = [])
     {
+        header(self::_build_http_header_string(200));
+        header("Content-Type: application/json");
         return json_encode([
             'status' => 'success',
             'msg'    => $msg,
